e6a2a4cc6ea69591051437310480949ffe6417ba
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2024-09-13 13:35:05 -0600

    Added change.py file - Added main function with coin denominations - Added find_combinations and display_results functions, missing logic - Added student information

---
+++ A0/bin/change.py
@@ -0,0 +1,22 @@
+"""
+Name: Connor Morrison
+NSID: tvi340
+Student Number: 11374770
+Course: CMPT 263
+"""
+
+# Find coin combinations
+def find_combinations(amount, coins):
+    pass
+
+def display_results():
+    pass
+
+def main():
+    amount = int(input(""))
+    coins = [1, 10, 25, 50, 100, 200]
+
+    find_combinations(amount, coins)
+
+if __name__ == "__main__":
+    main()
