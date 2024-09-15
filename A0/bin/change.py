beca8de7f794ea57cbdbc9cbb5d8bbe2b6a49c15
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2024-09-15 00:03:21 -0600

    Continued implementation of 'calculate_combinations' function - Completed recursive case, subtracting coin denomination from amount and appending to list

--- A0/bin/change.py
+++ A0/bin/change.py
@@ -5,29 +5,50 @@ Student Number: 11374770
 Course: CMPT 263
 """
 
-# Find coin combinations
-# def calculate_combinations(amount, coins, current_index=0):
-#     # base case
-#     if amount == 0:
-#         display_results(amount)
-#         return None
+
+# Calculate coin combinations
+def calculate_combinations(amount, coins, current_combinations, current_index):
+    # Base case
+    if amount == 0:
+        display_combinations(current_combinations)
+        return None
     
     # Else we have more than 0 for the amount, use recursion
+    for i in range(current_index, len(coins)):
+        if coins[i] <= amount:
+            # Subtracts coins[i] quantity from amount
+            # Appends coins[i] to current_combinations
+            calculate_combinations(amount - coins[i], coins, current_combinations + [coins[i]], i)
 
 
-def display_combinations(combination):
+# Display coin combinations
+def display_combinations(current_combinations):
     result = {}
-    for coin in combination:
-        result[coin] += 
+    for coin in current_combinations:
+        result[coin] = result.get(coin, 0) + 1
+    
+    # Print in format
+    formatted_strings = []
+    for coin, count in result.items():
+        formatted_string = f"{count}@{coin}"
+        formatted_strings.append(formatted_string)
+    
+    output = ", ".join(formatted_strings)
+    print(output)
 
 
 def main():
     amount = int(input(""))
-    coins = [1, 10, 25, 50, 100, 200]
+    coins = [200, 100, 50, 25, 10, 5, 1]
 
-    # calculate_combinations(amount, coins)
-    combination = 
-    display_combinations(combination)
+    # Special case where amount = 0
+    if amount == 0:
+        print("0@0")
+        return None
+
+    # Calculate results
+    calculate_combinations(amount, coins, current_combinations=[], current_index=0)
+
 
 if __name__ == "__main__":
     main()
