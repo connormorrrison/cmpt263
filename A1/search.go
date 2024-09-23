08359fe671b14ccc2d9b35bc118e7d18b9ef306c
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2024-09-23 13:04:57 -0600

    Added user input reading in main function

--- A1/search.go
+++ A1/search.go
@@ -5,7 +5,22 @@ Student Number: 11374770
 Course: CMPT 263
 */
 
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
 // Get user input
 func main() {
-	
+	var numStudentNumbers int ;
+	var numQueries int ;
+
+	// Read in user input
+	_, err := fmt.Scanf("%d %d", &numStudentNumbers, &numQueries) ;
+	if err != nil {
+		os.Exit(1) ;
+	}
+
 }
