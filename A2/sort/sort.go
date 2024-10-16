8efecf805beb363addb58137e6ecfbcb41deecd8
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2024-10-16 13:51:25 -0600

    Updated project structure to contain sort and sparse packages

---
+++ A2/sort/sort.go
@@ -0,0 +1,13 @@
+/*
+Name: Connor Morrison
+NSID: tvi340
+Student Number: 11374770
+Course: CMPT 263
+*/
+
+package sort
+
+func isort(array [] int) []int {
+	// Implement later
+	return array;
+}
---
+++ A2/sparse/find.go
@@ -0,0 +1,13 @@
+/*
+Name: Connor Morrison
+NSID: tvi340
+Student Number: 11374770
+Course: CMPT 263
+*/
+
+package sparse
+
+func find([]int, int) (bool, int) {
+	// Implement later
+	return false, -1
+}
