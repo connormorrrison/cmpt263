702de9233cabace95641e8795aefd4ab9edd7f06
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2024-10-16 10:22:48 -0600

    Added variety of test cases 1-10

---
+++ A2/ints/ints.go
@@ -0,0 +1,20 @@
+/*
+Name: Connor Morrison
+NSID: tvi340
+Student Number: 11374770
+Course: CMPT 263
+*/
+
+package ints ;
+
+import (
+	"fmt" ;
+	"os" ;
+)
+
+// ReadInts reads a sequence of integers from user input
+func ReadInts() []int {
+
+	// Read in user input
+	_, err := input("")
+}
