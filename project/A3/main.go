17fe270bd4e07080193bc0439ba7fa16f41d4169
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2024-12-01 21:14:36 -0600

    Updated main.go to include updated Node variant definitions and NodeOperations interface
    - Node variants are separated into distinct type definitions
    - Node operations interface defined

--- project/A3/main.go
+++ project/A3/main.go
@@ -11,12 +11,26 @@ type Monster struct {
 	AttackRating int;
 }
 
-// Define NodeType for const expression
-type NodeType int;
+// Node variants
+type Head struct {
+	next *Node;
+}
 
-const (
-	HEAD NodeType = iota
-	NODE
-	TAIL
-)
+type Node struct {
+	data Monster;
+	next *Node;
+	prev *Node;
+}
 
+type Tail struct {
+	prev *Node;
+}
+
+type NodeOperations interface {
+	Prepend(m Monster);  // Adds the data element to the beginning of the dequeue
+	Append(m Monster);  // Adds the data element to the end of the dequeue
+	Len() int;  // Returns an int telling us how many elements are in the dequeue
+	Get(i int) (bool, Monster);  // Returns the Monster at position i
+	Shift();  // To remove at the front
+	Drop();  // To remove at the back
+}
