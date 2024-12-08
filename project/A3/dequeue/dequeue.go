2cff1860557895b8714db2a5c1ca0648be269cd8
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2024-12-08 05:26:24 -0600

    Updated main.go, dequeue.go, and removed test files
    - Removed test files as testing will now be done in main() in main.go
    - Created sample Monster for testing
    - Implemented MakeDequeue func to initialize a new dequeue with HEAD and TAIL
    - Added placeholder methods for HEAD, NODE and TAIL

--- project/A3/dequeue/dequeue.go
+++ project/A3/dequeue/dequeue.go
@@ -1,38 +1,137 @@
-package main;
+package dequeue;
 
-// Each node contains a MONSTER data type
+// Monster data type
 type Monster struct {
-	Name string;  // Non-empty, no whitespace
-	InitiativeModifier int;  // (-20, 20)
-	ChallengeRating int;  // > 0
-	ArmourClass int;  // >= 0
-	AttackRating int;  // >= -20
+	Name string; // Non-empty, no whitespace
+	InitiativeModifier int; // (-20, 20)
+	ChallengeRating int; // > 0
+	ArmourClass int; // >= 0
+	AttackRating int; // >= -20
 }
 
-// Node variants
-type Head struct {
-	next *Node;
+// Dequeue interface defines the operations on the structure
+type Dequeue interface {
+	Prepend(m Monster) Dequeue;
+	Append(m Monster) Dequeue;
+	Len() int;
+	Get(i int) (bool, Monster);
+	Shift();
+	Drop();
 }
 
-type Node struct {
+// HEAD variant: start of dequeue
+type HEAD struct {
+	next Dequeue;
+}
+
+// NODE variant: contains Monster and links prev and next nodes
+type NODE struct {
 	data Monster;
-	next *Node;
-	prev *Node;
+	prev Dequeue;
+	next Dequeue;
 }
 
-type Tail struct {
-	prev *Node;
+// TAIL variant: end of dequeue
+type TAIL struct {
+	prev Dequeue;
 }
 
-type NodeOperations interface {
-	Prepend(m Monster);  // Adds the data element to the beginning of the dequeue
-	Append(m Monster);  // Adds the data element to the end of the dequeue
-	Len() int;  // Returns an int telling us how many elements are in the dequeue
-	Get(i int) (bool, Monster);  // Returns the Monster at position i
-	Shift();  // To remove at the front
-	Drop();  // To remove at the back
+func MakeDequeue() Dequeue {
+	head := &HEAD{};
+	tail := &TAIL{};
+	head.next = tail;
+	tail.prev = head;
+	return head;
 }
 
-type Dequeue struct {
-	
+// HEAD methods
+
+func (h *HEAD) Prepend(m Monster) Dequeue {
+	// TODO
+	return h; // return h just to compile
+}
+
+func (h *HEAD) Append(m Monster) Dequeue {
+	// TODO
+	return h;
+}
+
+func (h *HEAD) Len() int {
+	// TODO
+	return 0;
+}
+
+func (h *HEAD) Get(i int) (bool, Monster) {
+	// TODO
+	return false, Monster{};
+}
+
+func (h *HEAD) Shift() {
+	// TODO
+}
+
+func (h *HEAD) Drop() {
+	// TODO
+}
+
+
+// TAIL methods
+
+func (t *TAIL) Prepend(m Monster) Dequeue {
+	// TODO
+	return t;
+}
+
+func (t *TAIL) Append(m Monster) Dequeue {
+	// TODO
+	return t;
+}
+
+func (t *TAIL) Len() int {
+	// TODO
+	return 0;
+}
+
+func (t *TAIL) Get(i int) (bool, Monster) {
+	// TODO
+	return false, Monster{};
+}
+
+func (t *TAIL) Shift() {
+	// TODO
+}
+
+func (t *TAIL) Drop() {
+	// TODO
+}
+
+
+// NODE methods
+
+func (n *NODE) Prepend(m Monster) Dequeue {
+	// TODO
+	return n;
+}
+
+func (n *NODE) Append(m Monster) Dequeue {
+	// TODO
+	return n;
+}
+
+func (n *NODE) Len() int {
+	// TODO
+	return 1 + n.next.Len(); // example logic (once next is implemented)
+}
+
+func (n *NODE) Get(i int) (bool, Monster) {
+	// TODO
+	return false, Monster{};
+}
+
+func (n *NODE) Shift() {
+	// TODO
+}
+
+func (n *NODE) Drop() {
+	// TODO
 }
--- project/A3/main.go
+++ project/A3/main.go
@@ -1,5 +1,26 @@
 package main;
 
+import (
+    "fmt";
+	"A3/dequeue";
+)
+
+
 func main() {
-	
+	// Create sample monsters
+	m1 := dequeue.Monster{Name: "Godzilla", InitiativeModifier: 0, ChallengeRating: 1, ArmourClass: 13, AttackRating: -1};
+	m2 := dequeue.Monster{Name: "Bigfoot", InitiativeModifier: 2, ChallengeRating: 2, ArmourClass: 15, AttackRating: 0};
+	m3 := dequeue.Monster{Name: "Dracula", InitiativeModifier: 5, ChallengeRating: 0, ArmourClass: 18, AttackRating: 5};
+
+	dq := dequeue.MakeDequeue();
+
+	// TEST Append(), Prepend()
+	dq = dq.Append(m1);
+	dq = dq.Append(m2);
+	dq = dq.Prepend(m3);
+
+	// TEST Len()
+	fmt.Println("Len:", dq.Len());
+
+
 }
